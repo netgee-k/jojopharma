aa2dff00f3864b754f67bfc198194af643ebbdee
kimani <92677525+netgee-k@users.noreply.github.com>
2025-05-12 04:23:47 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "", "jojo_meds");
+$conn = new mysqli("localhost", "jojopharma", "", "jojopharma");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
